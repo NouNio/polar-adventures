cc87bc63c8559f7c397857de0f1e30eac2c6c069
glennDittmann <glenn.dittmann@posteo.de>
2022-03-10 00:03:43 +0100

    add incude folder with custom Shader and Camera header only classes

---
+++ include/Camera.h
@@ -0,0 +1,149 @@
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#include <GL/glew.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include <vector>
+
+
+enum Camera_Movement {
+    FORWARD,
+    BACKWARD,
+    LEFT,
+    RIGHT
+};
+
+// Default camera values
+const float YAW = -90.0f;           // angle that describes how much the camera looks left or right
+const float PITCH = 0.0f;           // angle that describes how much the camera looks up or down
+const float MAX_PITCH = 89.0f;
+const float MAX_ZOOM = 45.0f;
+const float MIN_ZOOM = 1.0f;
+const float SPEED = 2.5f;
+const float SENSITIVITY = 0.1f;
+const float ZOOM = 45.0f;
+
+
+class Camera
+{
+public:
+    // camera Attributes
+    glm::vec3 pos, front, up, right;
+    // world attribute
+    glm::vec3 worldUp;
+    // euler Angles
+    float yaw, pitch;
+    // camera options
+    float moveSpeed, mouseSensitivity, zoom;
+
+
+    Camera(glm::vec3 pos = glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f), float yaw = YAW, float pitch = PITCH) : front(glm::vec3(0.0f, 0.0f, -1.0f)), moveSpeed(SPEED), mouseSensitivity(SENSITIVITY), zoom(ZOOM)
+    {
+        this->pos = pos;
+        this->worldUp = up;
+        this->yaw = yaw;
+        this->pitch = pitch;
+        updateCameraVectors();
+    }
+
+
+    Camera(float posX, float posY, float posZ, float upX, float upY, float upZ, float yaw, float pitch) :front(glm::vec3(0.0f, 0.0f, -1.0f)), moveSpeed(SPEED), mouseSensitivity(SENSITIVITY), zoom(ZOOM)
+    {
+        this->pos = glm::vec3(posX, posY, posZ);
+        this->worldUp = glm::vec3(upX, upY, upZ);
+        this->yaw = yaw;
+        this->pitch = pitch;
+        updateCameraVectors();
+    }
+
+   
+    glm::mat4 GetViewMatrix()
+    {
+        return glm::lookAt(this->pos,                   // position of the camera
+                           this->pos + this->front,     // target position, where should the camera look to, i.e. a vector pointing from pos to targetPos
+                           this->up);                   // represents "up" in world space
+    }
+
+
+    // processes input received from any keyboard-like input system. 
+    // Accepts input parameter in the form of camera defined ENUM (to abstract it from windowing systems) (I think this a very nice idea from Joey de Vries)
+    void processKeyboard(Camera_Movement direction, float deltaTime)
+    {
+        float velocity = this->moveSpeed * deltaTime;
+        if (direction == FORWARD)
+            this->pos += this->front * velocity;
+        if (direction == BACKWARD)
+            this->pos -= this->front * velocity;
+        if (direction == LEFT)
+            this->pos -= this->right * velocity;
+        if (direction == RIGHT)
+            this->pos += this->right * velocity;
+    }
+
+    
+    // processes input received as the offset in x and y w.r.t. the last measured mouse pos.
+    void processMouseMovement(float xoffset, float yoffset, GLboolean constrainPitch = true)
+    {
+        xoffset *= this->mouseSensitivity;
+        yoffset *= this->mouseSensitivity;
+
+        this->yaw += xoffset;   // update euler angles w.r.t new mouse pos
+        this->pitch += yoffset;
+
+        if (constrainPitch)  // if pitch (up / down angle) is woud leave bounds, the view would be inverted
+        {
+            if (this->pitch > MAX_PITCH)
+                this->pitch = MAX_PITCH;
+            if (this->pitch < -MAX_PITCH)
+                this->pitch = -MAX_PITCH;
+        }
+
+        updateCameraVectors();
+    }
+
+
+    // process mouse input received from the vertical wheel-axis and clip any exceeding values
+    void processMouseScroll(float yoffset)
+    {
+        this->zoom -= (float)yoffset;
+        if (this->zoom < MIN_ZOOM)
+            this->zoom = MIN_ZOOM;
+        if (this->zoom > MAX_ZOOM)
+            this->zoom = MAX_ZOOM;
+    }
+
+private:
+    // update the camera vectors, i.e. FRONT, UP, RIGHT. Note that order matters here, as the UP values uses the new RIGHT value & RIGHT uses the new FRONT.
+    void updateCameraVectors()
+    {
+        updateFrontVector();
+        updateRightVector();
+        updateUpVector();
+    }
+
+
+    void updateFrontVector()  // update the cameras front vector using eulers formula
+    {
+        this->front = glm::normalize(glm::vec3( cos(glm::radians(this->yaw)) * cos(glm::radians(this->pitch)),
+                                                sin(glm::radians(this->pitch)),
+                                                sin(glm::radians(this->yaw)) * cos(glm::radians(this->pitch)) ));
+    }
+
+
+    void updateRightVector()
+    {
+        this->right = glm::normalize(glm::cross(this->front, this->worldUp));  // normalize, cause the length gets closer to 0 the more you look up or down, which would result in slower movement
+    }
+
+
+    void updateUpVector()
+    {
+        this->up = glm::normalize(glm::cross(this->right, this->front));
+    }
+};
+
+#endif
+
---
+++ include/Shader.h
@@ -0,0 +1,201 @@
+#ifndef SHADER_H
+#define SHADER_H
+
+#include <GL/glew.h>  // one could also use GLAD here
+#include <glm/glm.hpp>
+
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+using namespace std;
+
+// this code is inspired by learnopengl.com as this is my main resource for opengl information
+// other resources my have been influential as well, this file howver should describes my way of implementing the theoretical concepts
+
+// the procedure can roughly be described in 4 steps
+// 1. load the shader files and extract the code
+// 2. compile the shaders
+// 3. attach the shaders to the program and link the program
+// 4. delete the shaders
+
+// constants
+#define INFO_LOG_SIZE 1024
+const string VERTEX = "VERTEX";
+const string FRAGMENT = "FRAGMENT";
+const string PROGRAM = "PROGRAM";
+const string SEPARATOR = "\n -- --------------------------------------------------- -- ";
+
+class Shader
+{
+public:
+    unsigned int ID;
+
+    Shader(const char* vertexShaderPath, const char* fragmentShaderPath)
+    {
+        // 1. first we need to read the source code in the shader files, basically char by char
+        string vertexShaderCode = readShaderFile(vertexShaderPath);
+        string fragmentShaderCode = readShaderFile(fragmentShaderPath);
+        
+        // 2. compile the shaders and 3. link the program (+ 4. delete the shaders)
+        compileShadersAndLinkProgram(vertexShaderCode.c_str(), fragmentShaderCode.c_str());
+    }
+
+
+    void use() const
+    {
+        glUseProgram(this->ID);
+    }
+
+
+    // the following functions are for setting respective uniforms in the shader, by specifing the name and value of the uniform to take
+    void setBool(const string& name, bool val) const
+    {
+        glUniform1i(glGetUniformLocation(this->ID, name.c_str()), (int)val); // transform val to 0 or 1
+    }
+
+
+    void setInt(const string& name, int val) const
+    {
+        glUniform1i(glGetUniformLocation(this->ID, name.c_str()), val);
+    }
+
+
+    void setFloat(const string& name, float val) const
+    {
+        glUniform1f(glGetUniformLocation(this->ID, name.c_str()), val);
+    }
+
+
+    void setVec2(const string& name, const glm::vec2& val) const
+    {
+        glUniform2fv(glGetUniformLocation(this->ID, name.c_str()), 1, &val[0]);
+    }
+
+
+    void setVec2(const string& name, float x, float y) const
+    {
+        glUniform2f(glGetUniformLocation(this->ID, name.c_str()), x, y);
+    }
+
+
+    void setVec3(const string& name, const glm::vec3& val) const
+    {
+        glUniform3fv(glGetUniformLocation(this->ID, name.c_str()), 1, &val[0]);
+    }
+
+
+    void setVec3(const string& name, float x, float y, float z) const
+    {
+        glUniform3f(glGetUniformLocation(this->ID, name.c_str()), x, y, z);
+    }
+
+
+    void setVec4(const string& name, const glm::vec4& val) const
+    {
+        glUniform4fv(glGetUniformLocation(this->ID, name.c_str()), 1, &val[0]);
+    }
+
+
+    void setVec4(const string& name, float x, float y, float z, float w) const
+    {
+        glUniform4f(glGetUniformLocation(this->ID, name.c_str()), x, y, z, w);
+    }
+
+
+    void setMat2(const string& name, const glm::mat2& mat) const
+    {
+        glUniformMatrix2fv(glGetUniformLocation(this->ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+    }
+
+
+    void setMat3(const string& name, const glm::mat3& mat) const
+    {
+        glUniformMatrix3fv(glGetUniformLocation(this->ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+    }
+
+
+    void setMat4(const string& name, const glm::mat4& mat) const
+    {
+        glUniformMatrix4fv(glGetUniformLocation(this->ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+    }
+
+private:
+    // read the shader file and return the contained source code
+    string readShaderFile(const char* shaderPath)
+    {
+        string shaderCode;
+        ifstream shaderFile;
+        stringstream shaderStream;
+        shaderFile.exceptions(ifstream::failbit | ifstream::badbit);  // make ifstream object throw the respective exceptions
+
+        try  // try to open the specified file
+        {
+            shaderFile.open(shaderPath);
+            shaderStream << shaderFile.rdbuf();  // transfer the file buffers into the stream objects
+            shaderFile.close();
+        }
+        catch (ifstream::failure& e)
+        {
+            cout << "There was a problem reading this file: " << e.what() << endl;
+        }
+        return shaderStream.str();  // now we can transform the stream object into string objects
+    }
+
+
+    // compile the shaders, link to the program and delete the shaders
+    void compileShadersAndLinkProgram(const char* vertexShaderPtr, const char* fragmentShaderPtr)
+    {
+        // 2. compile the shaders
+        unsigned int vertex, fragment;
+        // vertex shader
+        vertex = glCreateShader(GL_VERTEX_SHADER);          // create openGL shader
+        glShaderSource(vertex, 1, &vertexShaderPtr, NULL);  // give it the shader code from the shader file
+        glCompileShader(vertex);                            // compile
+        checkPrintCompileErrors(vertex, VERTEX);            // check errors
+        // fragment Shader
+        fragment = glCreateShader(GL_FRAGMENT_SHADER);
+        glShaderSource(fragment, 1, &fragmentShaderPtr, NULL);
+        glCompileShader(fragment);
+        checkPrintCompileErrors(fragment, FRAGMENT);
+
+        // 3. attach and link the shaders to the shader Program
+        this->ID = glCreateProgram();
+        glAttachShader(this->ID, vertex);
+        glAttachShader(this->ID, fragment);
+        glLinkProgram(this->ID);
+        checkPrintCompileErrors(this->ID, PROGRAM);
+
+        // 4. we can delete the shaders now (after linking)
+        glDeleteShader(vertex);
+        glDeleteShader(fragment);
+    }
+
+
+    // check and print comile errors based on the given thrown error
+    void checkPrintCompileErrors(GLuint shader, string type)
+    {
+        GLint success;
+        GLchar infoLog[INFO_LOG_SIZE];
+        if (type != PROGRAM)
+        {
+            glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+            if (!success)
+            {
+                glGetShaderInfoLog(shader, INFO_LOG_SIZE, NULL, infoLog);
+                cout << "There was an error compiling this shader: " << type << "\n" << infoLog << SEPARATOR << endl;
+            }
+        }
+        else
+        {
+            glGetProgramiv(shader, GL_LINK_STATUS, &success);
+            if (!success)
+            {
+                glGetProgramInfoLog(shader, INFO_LOG_SIZE, NULL, infoLog);
+                cout << "There was an error linking the program: " << type << "\n" << infoLog << SEPARATOR << endl;
+            }
+        }
+    }
+};
+#endif
+
