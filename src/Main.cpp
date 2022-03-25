6beb0bf7de8db6d22753a7df3542e87dbf0cabb1
glennDittmann <glenn.dittmann@posteo.de>
2022-03-26 00:59:21 +0100

    refactor code (e.g. draw player object), alter camera callbacks for player obj movement, ...

--- src/Main.cpp
+++ src/Main.cpp
@@ -25,15 +25,23 @@
 #include <Constants.h>
 
 
+// testing
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
+
 // prototypes
 GLFWwindow* initGLFWandGLEW();
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
-void processInput(GLFWwindow* window);
+void processInput(GLFWwindow* window, glm::vec3* playerTranslation);
 void setPointLightShaderParameters(Shader& shader, string pointLightNumber, glm::vec3 postion);
 void computeTimeLogic();
 void drawGameObject(Model* gameObj, glm::vec3 translation, float angle, glm::vec3 rotationAxes, glm::vec3 scale, Shader* shader);
+void drawPlayer(Model* gameObj, glm::vec3 translation, float angle, glm::vec3 rotationAxes, glm::vec3 scale, Shader* shader);
+void activateShader(Shader* shader);
 
 // settings
 const unsigned int SCR_WIDTH = 800;
@@ -42,6 +50,7 @@ bool showHUD = false;
 
 // camera
 Camera camera(glm::vec3(75.0f, 7.0f, 20.0f));
+glm::vec3 playerOffset(0.0, -6.0, -8.0); // the player is a little bit below and in front of the camera
 // lighting
 glm::vec3 directLightPos(30.f, 36.0f, 10.0f);
 float lastX = SCR_WIDTH / 2.0f;
@@ -60,12 +69,14 @@ int main(void)
 {
     GLFWwindow* window = initGLFWandGLEW();
 
-    stbi_set_flip_vertically_on_load(true);  // tell stb_image.h to flip loaded texture's on the y-axis (before loading model).
+    //stbi_set_flip_vertically_on_load(true);  // tell stb_image.h to flip loaded texture's on the y-axis (before loading model); NOTE turn off for most the textures I saw don't need it
     glEnable(GL_DEPTH_TEST);
     
     // load Shader, Models and HUD
     Shader lightShader(lightVertPath, lightFragPath);
     Shader modelShader(modelVertPath, modelFragPath);
+    Shader snowBallShader(snowBallVertPath, snowBallFragPath);
+    Model player(playerPath, true, PNG);
     Model heart(heartPath);
     Model tree(treePath);
     Model lightpost(lightpostPath);
@@ -75,20 +86,39 @@ int main(void)
     Model treePineSnow(treePineSnowPath);
     Model treePineSnowed(treePineSnowedPath);
     Model treePineSnowRound(treePineSnowRoundPath);
+    Model snowBall(snowBallPath, true, PNG);
     Model cubeWorld(worldPath);
+
+    Model isoGroundSnow(isoGroundSnowPath, true, GIF);
+    Model matchBox(matchBoxPath, true, PNG);
+    Model plinth(plinthPath, true, PNG);
+    Model campfire(campfirePath);
+    Model reptileMage(reptileMagePath, true, PNG);
+    Model bird(birdPath);
+    Model chick(chickPath);
+    Model fish(fishPath);
+    Model fox(foxPath);
+    Model whale(whalePath);
+    //Model stoneArray(stoneArrayPath, true, JPG);
+    //Model stoneArk(stoneArkPath);
+
     // do HUD stuff 
     Shader HUDShader(HUDVertPath, HUDFragPath);
     glm::mat4 projection = glm::ortho(0.0f, static_cast<float>(SCR_WIDTH), 0.0f, static_cast<float>(SCR_HEIGHT));
     HUDShader.use();
-    glUniformMatrix4fv(glGetUniformLocation(HUDShader.ID, "proj"), 1, GL_FALSE, glm::value_ptr(projection)); HUD hud(fontPath);
+    glUniformMatrix4fv(glGetUniformLocation(HUDShader.ID, "proj"), 1, GL_FALSE, glm::value_ptr(projection)); 
+    HUD hud(fontPath);
 
 
-    // do physics stuff  with bullet3
+    // do physics stuff with bullet3
     // TODO
 
 
     // render loop
     // -----------
+    glm::vec3 playerPos = camera.pos + playerOffset;
+    float playerRotAngle = 0.0f;
+    glm::vec3 playerRotationAxes(0.0f, 1.0f, 0.0f);
     while (!glfwWindowShouldClose(window))
     {
         // per-frame time logic
@@ -96,35 +126,15 @@ int main(void)
 
         // input
         // -----
-        processInput(window);
+        processInput(window, &playerPos);
 
         // render
         // ------
         glClearColor(0.6f, 0.7f, 0.9f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        // be sure to activate shader when setting uniforms/drawing objects
-        modelShader.use();
-        modelShader.setVec3("viewPos", camera.pos);
-        modelShader.setFloat("material.shininess", 32.0f);
-
-        /*
-           Here we set all the uniforms for the 5/6 types of lights we have. We have to set them manually and index
-           the proper PointLight struct in the array to set each uniform variable. This can be done more code-friendly
-           by defining light types as classes and set their values in there, or by using a more efficient uniform approach
-           by using 'Uniform buffer objects', but that is something we'll discuss in the 'Advanced GLSL' tutorial.
-        */
-        // directional light
-        modelShader.setVec3("directionalLight.direction", -20.2f, -21.0f, -20.3f);
-        modelShader.setVec3("directionalLight.ambient", 0.05f, 0.05f, 0.05f);
-        modelShader.setVec3("directionalLight.diffuse", 0.4f, 0.4f, 0.4f);
-        modelShader.setVec3("directionalLight.specular", 0.5f, 0.5f, 0.5f);
-
-        setPointLightShaderParameters(modelShader, "0", pointLightPositions[0]);  // point light 1
-        setPointLightShaderParameters(modelShader, "1", pointLightPositions[1]);  // point light 2
-        setPointLightShaderParameters(modelShader, "2", pointLightPositions[2]);  // point light 3
-        setPointLightShaderParameters(modelShader, "3", pointLightPositions[3]);  // point light 4
-        setPointLightShaderParameters(modelShader, "4", pointLightPositions[4]);  // point light 5
+        activateShader(&modelShader);
+        
 
         // view/projection transformations
         glm::mat4 projection = glm::perspective(glm::radians(camera.zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
@@ -152,9 +162,28 @@ int main(void)
         drawGameObject(&snowman, glm::vec3(26.0f, 0.0f, 3.0f), 90.0f, glm::vec3(0.0f, 1.0f, 0.0f), glm::vec3(1.0f, 1.0f, 1.0f), &modelShader);
         drawGameObject(&snowPatch, glm::vec3(25.0f, 0.0f, 5.0f), 0.0f, glm::vec3(0.0f, 1.0f, 0.0f), glm::vec3(0.8f, 0.8f, 0.8f), &modelShader);
 
+        
+        activateShader(&snowBallShader);
+        snowBallShader.setMat4("projection", projection);
+        snowBallShader.setMat4("view", view);
+        float scale = 0.05f;
+        drawGameObject(&plinth, glm::vec3(28.0f, 0.0f, 16.0f), 0.0f, glm::vec3(1.0f, 0.0f, 0.0f), glm::vec3(scale, scale, scale), &snowBallShader);
+
+        activateShader(&snowBallShader);
+        float scale2 = 0.4f;
+        playerOffset = camera.front * glm::length(playerOffset);
+        playerPos = camera.pos + playerOffset + glm::vec3(0.0f, -5.0f, -1.0f);
+        playerRotAngle = -camera.yaw;
+        drawPlayer(&player, playerPos, playerRotAngle, playerRotationAxes, glm::vec3(scale2, scale2, scale), &snowBallShader);
+
+        activateShader(&snowBallShader);
+        snowBallShader.setMat4("projection", projection);
+        snowBallShader.setMat4("view", view);
+        drawGameObject(&snowBall, glm::vec3(28.0f, 4.0f, 16.0f), 0.0f, glm::vec3(1.0f, 0.0f, 0.0f), glm::vec3(0.01f, 0.01f, 0.01f), &snowBallShader);
 
         // PHYSICS
         // TO DO
+        
 
         // DRAW HUD
         // HUD to render last so blending works properly, try it out in the beginning of the render loop ;)
@@ -162,11 +191,16 @@ int main(void)
         {
             hud.render(HUDShader, "Snowballs 0/4", 10.0f, 560.0f, 0.5f, glm::vec3(0.1f, 0.6f, 0.9f));
             hud.render(HUDShader, "--Data--", 10.0f, 530.0f, 0.5f, glm::vec3(0.1f, 0.6f, 0.9f));
-            hud.render(HUDShader, "Camera X: " + to_string(camera.pos.x), 10.0f, 510.0f, 0.5f, glm::vec3(0.1f, 0.6f, 0.9f));
-            hud.render(HUDShader, "Camera Y: " + to_string(camera.pos.y), 10.0f, 490.0f, 0.5f, glm::vec3(0.1f, 0.6f, 0.9f));
-            hud.render(HUDShader, "Camera Z: " + to_string(camera.pos.z), 10.0f, 470.0f, 0.5f, glm::vec3(0.1f, 0.6f, 0.9f));
-            hud.render(HUDShader, "FPS: " + to_string(FPS), 10.0f, 450.0f, 0.5f, glm::vec3(0.1f, 0.6f, 0.9f));
-            hud.render(HUDShader, "ms/frame: " + to_string(msPerFrame), 10.0f, 430.0f, 0.5f, glm::vec3(0.1f, 0.6f, 0.9f));
+            hud.render(HUDShader, "Camera.pos X: " + to_string(camera.pos.x), 10.0f, 510.0f, 0.5f, glm::vec3(0.1f, 0.6f, 0.9f));
+            hud.render(HUDShader, "Camera.pos Y: " + to_string(camera.pos.y), 10.0f, 490.0f, 0.5f, glm::vec3(0.1f, 0.6f, 0.9f));
+            hud.render(HUDShader, "Camera.pos Z: " + to_string(camera.pos.z), 10.0f, 470.0f, 0.5f, glm::vec3(0.1f, 0.6f, 0.9f));
+            hud.render(HUDShader, "Camera.front X: " + to_string(camera.front.x), 10.0f, 450.0f, 0.5f, glm::vec3(0.1f, 0.6f, 0.9f));
+            hud.render(HUDShader, "Camera.front Y: " + to_string(camera.front.y), 10.0f, 430.0f, 0.5f, glm::vec3(0.1f, 0.6f, 0.9f));
+            hud.render(HUDShader, "Camera.front Z: " + to_string(camera.front.z), 10.0f, 410.0f, 0.5f, glm::vec3(0.1f, 0.6f, 0.9f));
+            hud.render(HUDShader, "Camera.front len: " + to_string(glm::length(camera.front)), 10.0f, 390.0f, 0.5f, glm::vec3(0.1f, 0.6f, 0.9f));
+            hud.render(HUDShader, "playerOffset len: " + to_string(glm::length(playerOffset)), 10.0f, 370.0f, 0.5f, glm::vec3(0.1f, 0.6f, 0.9f));
+            hud.render(HUDShader, "FPS: " + to_string(FPS), 10.0f, 350.0f, 0.5f, glm::vec3(0.1f, 0.6f, 0.9f));
+            hud.render(HUDShader, "ms/frame: " + to_string(msPerFrame), 10.0f, 330.0f, 0.5f, glm::vec3(0.1f, 0.6f, 0.9f));
         }
 
 
@@ -216,7 +250,7 @@ GLFWwindow* initGLFWandGLEW()
 
 
 // process all input that is triggered by the keyboard
-void processInput(GLFWwindow* window)
+void processInput(GLFWwindow* window, glm::vec3* playerTranslation)
 {   
     
 
@@ -224,15 +258,15 @@ void processInput(GLFWwindow* window)
         glfwSetWindowShouldClose(window, true);
 
     if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-        camera.processKeyboard(FORWARD, deltaTime);
+        camera.processKeyboard(FORWARD, deltaTime, playerTranslation);
     if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-        camera.processKeyboard(BACKWARD, deltaTime);
+        camera.processKeyboard(BACKWARD, deltaTime, playerTranslation);
     if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-        camera.processKeyboard(LEFT, deltaTime);
+        camera.processKeyboard(LEFT, deltaTime, playerTranslation);
     if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-        camera.processKeyboard(RIGHT, deltaTime);
+        camera.processKeyboard(RIGHT, deltaTime, playerTranslation);
     if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS)
-        camera.processKeyboard(UP, deltaTime);
+        camera.processKeyboard(UP, deltaTime, playerTranslation);
 
 
     if (glfwGetKey(window, GLFW_KEY_H) == GLFW_PRESS && (glfwGetTime() - lastHUDPress) >= 0.2)
@@ -283,19 +317,6 @@ void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
 }
 
 
-// set the pointlights[] uniform values in the specified shader
-void setPointLightShaderParameters(Shader& shader, string pointLightNumber, glm::vec3 postion)
-{
-    shader.setVec3("pointLights[" + pointLightNumber + "].pos", postion);
-    shader.setVec3("pointLights[" + pointLightNumber + "].ambient", 0.05f, 0.05f, 0.05f);
-    shader.setVec3("pointLights[" + pointLightNumber + "].diffuse", 0.8f, 0.8f, 0.8f);
-    shader.setVec3("pointLights[" + pointLightNumber + "].specular", 1.0f, 1.0f, 1.0f);
-    shader.setFloat("pointLights[" + pointLightNumber + "].Kc", 1.0f);
-    shader.setFloat("pointLights[" + pointLightNumber + "].Kl", 0.09f);
-    shader.setFloat("pointLights[" + pointLightNumber + "].Kq", 0.032f);
-}
-
-
 // compute deltaTime, FPS and ms/Frame
 void computeTimeLogic()
 {
@@ -308,7 +329,7 @@ void computeTimeLogic()
 
 
 // renders a model, by specifiyng the shader as well as transformation matrices 
-void drawGameObject(Model* gameObj, glm::vec3 translation, float angle, glm::vec3 rotationAxes, glm::vec3 scale, Shader *shader)
+void drawGameObject(Model* gameObj, glm::vec3 translation, float angle, glm::vec3 rotationAxes, glm::vec3 scale, Shader *shader)    
 {
     shader->use();
     // world transformation
@@ -319,4 +340,65 @@ void drawGameObject(Model* gameObj, glm::vec3 translation, float angle, glm::vec
     shader->setMat4("model", model);
 
     gameObj->draw(*shader);
+}
+
+
+// player rendering is differtent, as the player obj needs to be rotated around the camera object space y-axes (not around the player obj space y-axes
+void drawPlayer(Model* gameObj, glm::vec3 translation, float angle, glm::vec3 rotationAxes, glm::vec3 scale, Shader* shader)
+{
+    glm::mat4 projection = glm::perspective(glm::radians(camera.zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+    shader->setMat4("projection", projection);
+    
+    glm::mat4 view = camera.GetViewMatrix();
+    shader->setMat4("view", view);
+
+    shader->use();
+    // world transformation
+    glm::mat4 model = glm::mat4(1.0f);
+    model = glm::translate(model, translation);
+    model = glm::rotate(model, glm::radians(angle+90.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+    model = glm::scale(model, scale);
+    shader->setMat4("model", model);
+
+    gameObj->draw(*shader);
+}
+
+
+// set some prominent vaues for the shader used fr most of the models in the scene
+void activateShader(Shader *shader)
+{
+    // be sure to activate shader when setting uniforms/drawing objects
+    shader->use();
+    shader->setVec3("viewPos", camera.pos);
+
+    /*
+       Here we set all the uniforms for the 5/6 types of lights we have. We have to set them manually and index
+       the proper PointLight struct in the array to set each uniform variable. This can be done more code-friendly
+       by defining light types as classes and set their values in there, or by using a more efficient uniform approach
+       by using 'Uniform buffer objects', but that is something we'll discuss in the 'Advanced GLSL' tutorial.
+    */
+    // directional light
+    shader->setVec3("directionalLight.direction", -20.2f, -21.0f, -20.3f);
+    shader->setVec3("directionalLight.ambient", 0.2f, 0.2f, 0.2f);
+    shader->setVec3("directionalLight.diffuse", 1.0f, 1.0f, 1.0f);              // change here for scene brightness
+    shader->setVec3("directionalLight.specular", 0.5f, 0.5f, 0.5f);
+
+    setPointLightShaderParameters(*shader, "0", pointLightPositions[0]);  // point light 1
+    setPointLightShaderParameters(*shader, "1", pointLightPositions[1]);  // point light 2
+    setPointLightShaderParameters(*shader, "2", pointLightPositions[2]);  // point light 3
+    setPointLightShaderParameters(*shader, "3", pointLightPositions[3]);  // point light 4
+    setPointLightShaderParameters(*shader, "4", pointLightPositions[4]);  // point light 5
+}
+
+
+// set the pointlights[] uniform values in the specified shader
+void setPointLightShaderParameters(Shader& shader, string pointLightNumber, glm::vec3 postion)
+{
+    shader.setVec3("pointLights[" + pointLightNumber + "].pos", postion);
+    shader.setVec3("pointLights[" + pointLightNumber + "].ambient", 0.05f, 0.05f, 0.05f);
+    shader.setVec3("pointLights[" + pointLightNumber + "].diffuse", 0.8f, 0.8f, 0.8f);
+    shader.setVec3("pointLights[" + pointLightNumber + "].specular", 1.0f, 1.0f, 1.0f);
+    shader.setFloat("pointLights[" + pointLightNumber + "].Kc", 1.0f);
+    shader.setFloat("pointLights[" + pointLightNumber + "].Kl", 0.09f);
+    shader.setFloat("pointLights[" + pointLightNumber + "].Kq", 0.032f);
 }
