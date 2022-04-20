5cde6d2c9c243af522697fef02afa00f3b83807f
glennDittmann <glenn.dittmann@posteo.de>
2022-04-21 00:15:48 +0200

    add base classes for animation of character

---
+++ include/AnimatedMesh.h
@@ -0,0 +1,16 @@
+#pragma once
+#ifndef ANIMATEDMESH_H
+#define ANIMATEDMESH_H
+
+#include <Mesh.h>
+
+class AnimatedMesh : Mesh {
+	AnimatedMesh() {
+		if (true)
+			std::cout << "to implement this" << std::endl;
+	}
+};
+
+
+#endif // !ANIMATEDMESH_H
+
